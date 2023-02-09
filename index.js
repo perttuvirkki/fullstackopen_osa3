70390f4c2675fdb4f78995c02beaf7acb3baf79b
Perttu Virkki <perttuvirkki@Perttus-MacBook-Pro.local>
2023-02-09 13:59:18 +0200

    testing

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 const app = express();
 
-const cors = require("cors");
-
-app.use(cors());
-
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
